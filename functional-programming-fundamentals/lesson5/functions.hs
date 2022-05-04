670d2a170222da7b2c524e142ddd421705798dbe
dujesa <duje.saric@partners.xarvio.com>
2022-05-04 18:24:57 +0200

    Implemented perfect and factor number functions.

--- functional-programming-fundamentals/lesson5/functions.hs
+++ functional-programming-fundamentals/lesson5/functions.hs
@@ -10,3 +10,9 @@ positions x xs = [i | (x', i) <- zip xs [0..n], x == x']
   where n = length xs - 1
 
 pythagorean z = [(x, y, z) | x <- [1..z], y <- [1..z], x*x + y*y == z*z]
+
+factors :: Int -> [Int]
+factors x = [y | y <- [1..x-1], x`mod`y==0]
+
+perfect :: Int -> [Int]
+perfect x = [y | y <- [1..x-1], sum(factors y) == y]
